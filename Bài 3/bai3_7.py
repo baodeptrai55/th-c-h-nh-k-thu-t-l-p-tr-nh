106127da5fa20b38bf3b102bf4a6f1f9a268873f
baodeptrai55 <daubao19032005@gmail.com>
2025-03-21 08:04:09 +0700

    Add files via upload

---
+++ Bài 3/bai3_7.py
@@ -0,0 +1,8 @@
+def checkValue(n):
+    if n%2==0:
+        print("Đây là số chẵn")
+    else:
+        print("Đây là số lẻ")
+checkValue(7)
+print("Sinh viên: Đậu Văn Quốc Bảo")
+print(" MSSV: 235752020710014")
