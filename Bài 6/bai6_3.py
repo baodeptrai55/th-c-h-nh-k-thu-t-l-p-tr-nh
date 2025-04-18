81d2a10e50352b493ff36ad7552f38b83ea70c7c
baodeptrai55 <daubao19032005@gmail.com>
2025-04-18 10:42:33 +0700

    Add files via upload

---
+++ Bài 6/bai6_3.py
@@ -0,0 +1,19 @@
+class Nguoi(object):
+    def getGender(self):
+        return "Unknown"
+
+class Nam(Nguoi):
+    def getGender(self):
+        return "Nam"
+
+class Nu(Nguoi):
+    def getGender(self):
+        return "Nữ"
+
+aNam = Nam()
+aNu = Nu()
+
+print(aNam.getGender())  
+print(aNu.getGender())   
+print("sinh vien: Đậu Văn Quốc Bảo")
+print("Mssv: 235752020710014")
