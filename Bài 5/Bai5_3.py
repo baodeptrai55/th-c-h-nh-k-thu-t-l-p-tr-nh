4b3be6579fa37c93ae3c64003014edf048fbebda
baodeptrai55 <daubao19032005@gmail.com>
2025-04-18 10:34:19 +0700

    Add files via upload

---
+++ Bài 5/Bai5_3.py
@@ -0,0 +1,5 @@
+print("Đậu Văn Quốc Bảo")
+print("MSSV:235752020710014")
+import numpy as np 
+x = np.arange(12, 38) 
+print(x) 
---
+++ Bài 5/Bai5_4.py
@@ -0,0 +1,9 @@
+print("Đậu Văn Quốc Bảo")
+print("MSSV:235752020710014")
+
+import numpy as np
+mang = np.arange(12, 39)
+print("Mảng ban đầu:",mang)
+
+mang_dao_nguoc = np.flip(mang)
+print("Mảng đảo ngược:", mang_dao_nguoc)
